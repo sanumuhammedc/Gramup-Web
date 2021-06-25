b37627789767c64158aa803188fe2808de4b7a87
sunith vs <sunithvazhenkada@gmail.com>
2021-06-25 20:56:02 +0530

    file edited

--- web/react/App.tsx
+++ web/react/App.tsx
@@ -10,7 +10,7 @@ export const App = ({wb}: { wb: Workbox; }) =>
     return (
         <>
             <HandleAppState wb={wb} /> 
-            <h1>React TypeScript Webpack Starter Template</h1>
+            <h1>edited</h1>
             <Counter />
             <img src={logo.src} srcSet={logo.srcSet} width={logo.width} height={logo.height} loading="lazy" alt="React logo" />
             <img src={gif} loading="lazy" alt="Silicon valley gif" />
